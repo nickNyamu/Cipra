d0b8e72298bc96ca1748bdfb13e9ca605f68ff46
NicholasWarui <nyamunick99@gmail.com>
2022-05-30 13:31:00 +0300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "icon" href = "{{asset ('img/crypto.png')}}">
+        <title>Cipra</title>
+
+        ✎
+
+        <img src="{{asset ('img/crypto.png')}}" alt="description" height="" width="" />
+
+        
+    </head>
+    <body class="antialiased">
+            <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" >
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" ></script>
+
+    </body>
+</html>
